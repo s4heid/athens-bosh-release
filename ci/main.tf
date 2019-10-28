8a6b0591eb9bc22179ce41bd499ecb5d086a808d
Sebastian Heid <sebastian.heid45@gmail.com>
2019-10-28 15:51:48 +0100

    Add git package; remove pre-start script
     * add terraform resources to create an s3 blobstore
     * check whether the catalog endpoint exposes the dependencies

--- ci/main.tf
+++ ci/main.tf
@@ -1,57 +1,148 @@
 variable "region" {
-  type = "string"
+  type    = string
   default = "eu-central-1"
-}
-variable "aws_access_key" {
-  type = "string"
-  description = "access key of aws account"
-}
-variable "aws_secret_key" {
-  type = "string"
-  description = "secret key of aws account"
 }
 
 variable "env_name" {
-  type    = "string"
+  type = string
 }
 
 variable "vpc_id" {
-  type    = "string"
+  type = string
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.region}"
+  region     = var.region
 }
 
+#
+# iam
+#
+
+resource "aws_iam_user" "ci" {
+  name = "${var.env_name}-ci"
+}
+
+resource "aws_iam_access_key" "ci" {
+  user = aws_iam_user.ci.name
+}
+
+resource "aws_iam_user_policy" "ci_ro" {
+  name = "${var.env_name}-policy"
+  user = "${aws_iam_user.ci.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:*",
+        "iam:*",
+        "s3:*",
+        "elasticloadbalancing:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+output "athens_access_key" {
+  value = aws_iam_access_key.ci.id
+}
+
+output "athens_secret_key" {
+  value     = aws_iam_access_key.ci.secret
+  sensitive = true
+}
+
+#
+# vpc
+#
+
 resource "aws_eip" "athens_eip" {
-  vpc        = true
+  vpc = true
 }
 
 resource "aws_security_group" "athens_sg" {
   name        = "athens_sg"
   description = "Allow TLS inbound traffic on port 3000"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
+    from_port = 3000
+    to_port   = 3000
+    protocol  = "tcp"
   }
 
   egress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 output "external_ip" {
-  value = "${aws_eip.athens_eip.public_ip}"
+  value = aws_eip.athens_eip.public_ip
 }
 
 output "athens_security_group" {
-  value = "${aws_security_group.athens_sg.id}"
+  value = aws_security_group.athens_sg.id
+}
+
+#
+# s3
+#
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.env_name}-blobs"
+  versioning {
+    enabled = true
+  }
+}
+
+data "aws_iam_policy_document" "public_read" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+    effect = "Allow"
+    principals {
+      type = "*"
+      identifiers = [
+        "*",
+      ]
+    }
+    resources = [
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.public_read.json
+}
+
+data "aws_iam_policy_document" "ci_s3" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "ci_s3" {
+  name   = "s3"
+  user   = aws_iam_user.ci.name
+  policy = data.aws_iam_policy_document.ci_s3.json
 }
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
-info () { printf "\033[00;34m[$(date +"%r")] $1\033[0m\n"; }
+info () { printf "\n\033[00;34m[$(date +"%r")] $1\033[0m\n"; }
 
 export BOSH_DEPLOYMENT=athens
 export BOSH_NON_INTERACTIVE=true
@@ -12,10 +12,10 @@ stemcell_name="${STEMCELL_NAME:-bosh-warden-boshlite-ubuntu-xenial-go_agent}"
 stemcell_version="${STEMCELL_VERSION:-456.30}"
 stemcell_sha1="${STEMCELL_SHA1:-61791b4d37ee3aacb9db36bb819c1fbcc4785a9e}"
 stemcell_os="${STEMCELL_OS:-ubuntu-xenial}"
-infra="${BBL_IAAS:-lite}"
+terraform_state_dir="ci/terraform.tfstate"
 
 ops_files=()
-if [ "$infra" = "aws" ]; then
+if [ "$BBL_IAAS" = "aws" ]; then
     ops_files+=( "--ops-file=./manifests/athens-terraform-ops.yml" )
 fi
 
@@ -34,7 +34,7 @@ info "Uploading cloud config"
 bosh update-config \
     --type=cloud \
     --name=athens \
-    --vars-file=<(terraform output -state=ci/terraform.tfstate -json) \
+    --vars-file=<(terraform output -state=$terraform_state_dir -json) \
     --var=default_security_group="$( bosh cloud-config | bosh int --path=/networks/name=default/subnets/0/cloud_properties/security_groups/0 - )" \
     ./manifests/cloud-config.yml \
 
@@ -46,7 +46,28 @@ bosh deploy \
     "${ops_files[@]}" \
     ./manifests/athens.yml
 
+info "Run test application"
+go get github.com/s4heid/walkthrough
+go clean -modcache
+
+host="$(terraform output -state="$terraform_state_dir" -json | jq -r .external_ip.value)"
+export GOPROXY="http://${host}:3000"
+export GO111MODULE=on
+
+pushd "$(go env GOPATH)"/src/github.com/s4heid/walkthrough
+go run .
+popd
+
+disk_version="$(curl -s "http://${host}:3000/catalog" | jq -r '.modules[]|select(.module=="github.com/athens-artifacts/samplelib").version')"
+if [ "$disk_version" != "v1.0.0" ]; then
+    >&2 echo "app dependencies not found on athens disk"
+    exit 1
+else
+    echo "found $disk_version on athens disk"
+fi
+
 info "Cleaning up"
 bosh delete-deployment
+bosh clean-up --all
 
 popd > /dev/null
